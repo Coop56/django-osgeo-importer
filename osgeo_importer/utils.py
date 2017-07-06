a41bc55d7a857be18af2ff4bc170ebac3d33fa3c
Coop56 <JCooper@jeremiahcooper.com>
2017-07-05 21:46:38 -0400

    Add function to reproject unsupported EPSG codes

--- osgeo_importer/utils.py
+++ osgeo_importer/utils.py
@@ -622,6 +622,74 @@ def convert_wkt_to_epsg(wkt, epsg_directory=settings.PROJECTION_DIRECTORY, force
         logger.error(msg)
         raise Exception(msg)
 
+    
+def reproject_coordinate_system(layer_name, in_shp_layer):
+    shp_driver = ogr.GetDriverByName('ESRI Shapefile')
+
+    # input SpatialReference
+    input_srs = in_shp_layer.GetSpatialRef()
+
+    # output SpatialReference
+    output_srs = osr.SpatialReference()
+    output_srs.ImportFromEPSG(4326)
+
+    # create the CoordinateTransformation
+    coord_trans = osr.CoordinateTransformation(input_srs, output_srs)
+
+    # create the output layer
+    output_shp_file = '%s.shp'.format(layer_name)
+    # check if output file exists if yes delete it
+    if os.path.exists(output_shp_file):
+        shp_driver.DeleteDataSource(output_shp_file)
+
+    # create a new Shapefile object
+    output_shp_dataset = shp_driver.CreateDataSource(output_shp_file)
+
+    # create a new layer in output Shapefile and define its geometry type
+    output_shp_layer = output_shp_dataset.CreateLayer('%s_4326'.format(layer_name), geom_type=ogr.wkbMultiPolygon)
+
+    # add fields to the new output Shapefile
+    # get list of attribute fields
+    # create new fields for output
+    in_layer_def = in_shp_layer.GetLayerDefn()
+    for i in range(0, in_layer_def.GetFieldCount()):
+        field_def = in_layer_def.GetFieldDefn(i)
+        output_shp_layer.CreateField(field_def)
+
+    # get the output layer's feature definition
+    output_layer_def = output_shp_layer.GetLayerDefn()
+
+    # loop through the input features
+    in_feature = in_shp_layer.GetNextFeature()
+    while in_feature:
+        # get the input geometry
+        geom = in_feature.GetGeometryRef()
+        # reproject the geometry
+        geom.Transform(coord_trans)
+        # create a new feature
+        output_feature = ogr.Feature(output_layer_def)
+        # set the geometry and attribute
+        output_feature.SetGeometry(geom)
+        for i in range(0, output_layer_def.GetFieldCount()):
+            output_feature.SetField(output_layer_def.GetFieldDefn(i).GetNameRef(), in_feature.GetField(i))
+        # add the feature to the shapefile
+        output_shp_layer.CreateFeature(output_feature)
+        # destroy the features and get the next input feature
+        output_feature.Destroy()
+        in_feature.Destroy()
+        in_feature = in_shp_layer.GetNextFeature()
+
+    # close the shapefiles
+    output_shp_dataset.Destroy()
+
+    spatialRef = osr.SpatialReference()
+    spatialRef.ImportFromEPSG(4326)
+
+    spatialRef.MorphToESRI()
+    prj_file = open('%s.prj'.format(layer_name), 'w')
+    prj_file.write(spatialRef.ExportToWkt())
+    prj_file.close()
+
 
 def database_schema_name():
     db_settings = db.connections[settings.OSGEO_DATASTORE].settings_dict
